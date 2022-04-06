e6e99106229d1b59d29a530dd692cf75861361f4
Yurii <ukucher@gmail.com>
2022-04-06 22:33:02 +0300

    bugfix custom properties

--- src/modal.js
+++ src/modal.js
@@ -1,10 +1,10 @@
 (() => {
-  const refs1 = {
-    openModal1Btn: document.querySelector('[data-modal-1-open]'),
-    openModal10Btn: document.querySelector('[data-modal-10-open]'),
-    closeModal1Btn: document.querySelector('[data-modal-1-close]'),
-    modal1: document.querySelector('[data-modal-1]'),
-  };
+  // const refs1 = {
+  //   openModal1Btn: document.querySelector('[data-modal-1-open]'),
+  //   openModal10Btn: document.querySelector('[data-modal-10-open]'),
+  //   closeModal1Btn: document.querySelector('[data-modal-1-close]'),
+  //   modal1: document.querySelector('[data-modal-1]'),
+  // };
 
   const refs2 = {
     openModal2Btn: document.querySelector('[data-modal-2-open]'),
@@ -24,19 +24,19 @@
     modal4: document.querySelector('[data-modal-4]'),
   };
 
-  refs1.openModal1Btn.addEventListener('click', toggleModal1);
-  refs1.openModal10Btn.addEventListener('click', toggleModal1);
+  // refs1.openModal1Btn.addEventListener('click', toggleModal1);
+  // refs1.openModal10Btn.addEventListener('click', toggleModal1);
   refs2.openModal2Btn.addEventListener('click', toggleModal2);
   refs3.openModal3Btn.addEventListener('click', toggleModal3);
   refs4.openModal4Btn.addEventListener('click', toggleModal4);
 
-  refs1.closeModal1Btn.addEventListener('click', toggleModal1);
+  // refs1.closeModal1Btn.addEventListener('click', toggleModal1);
   refs2.closeModal2Btn.addEventListener('click', toggleModal2);
   refs3.closeModal3Btn.addEventListener('click', toggleModal3);
   refs4.closeModal4Btn.addEventListener('click', toggleModal4);
 
   function toggleModal1() {
-    refs1.modal1.classList.toggle('is-hidden');
+    // refs1.modal1.classList.toggle('is-hidden');
   }
 
   function toggleModal2() {
